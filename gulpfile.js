8d7f972bc7f0a2692ef343daf55186ef200947e3
Ivan Popal <maincorp228@gmail.com>
2016-06-28 20:31:58 +0400

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,13 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
+var svgstore = require("gulp-svgstore");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -19,10 +26,35 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.reload({stream: true}));
+});
+
+gulp.task("svgstore", function () {
+  return gulp
+  .src("img/*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(gulp.dest("build/img/"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+  ]))
+
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -34,5 +66,26 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("img/*.svg", ["svgstore"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "images", "svgstore", fn);
 });
